81ee345ce67702e7e1a45f32f63ed6b95506334d
Patchara Kaewnissai <patchara331003@gmail.com>
2026-09-18 16:36:47 +0700

    create expenses page

---
+++ frontend/app/home/expenses/page.tsx
@@ -0,0 +1,15 @@
+export default function page() {
+  return (
+    <div
+        className="px-7 pt-8 pb-10"
+    >
+      <div>
+        <p
+            className="font-bold text-2xl"
+        >
+            รายการทั้งหมด
+        </p>
+      </div>
+    </div>
+  )
+}
